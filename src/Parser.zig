e9c6006e11218f783862cc8ace117f71e88ae415
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2025-01-23 22:42:19 +0100

    added some error reporting

--- src/Parser.zig
+++ src/Parser.zig
@@ -16,7 +16,7 @@ pub const Node = struct {
     start_index: usize,
     end_index: usize,
 
-    children: []const usize,
+    children: []const usize = ([0]usize{})[0..],
 
     pub fn deinit(self: Node) void {
         self.allocator.free(self.children);
@@ -31,11 +31,37 @@ pub const Tree = struct {
 
     pub fn deinit(self: Tree) void {
         self.allocator.free(self.source);
-        for (self.nodes) |node| {
-            node.deinit();
+        for (self.nodes) |n| {
+            n.deinit();
         }
         self.allocator.free(self.nodes);
     }
+
+    pub fn node(self: Tree, node_index: usize) Node {
+        return self.nodes[node_index];
+    }
+
+    pub fn nodeKind(self: Tree, node_index: usize) []const u8 {
+        return self.nodes[node_index].kind;
+    }
+
+    pub fn children(self: Tree, node_index: usize) []const usize {
+        return self.nodes[node_index].children;
+    }
+
+    pub fn chars(self: Tree, node_index: usize) []const u8 {
+        const n = self.nodes[node_index];
+        return self.source[n.start_index..n.end_index];
+    }
+};
+
+const ErrorContext = struct {
+    message: []const u8,
+    node: []const u8,
+    source_offset: usize,
+    char_count: usize,
+    regex_expr: []const u8 = "",
+    regex_offset: usize = 0,
 };
 
 grammar: Grammar,
@@ -43,6 +69,8 @@ allocator: std.mem.Allocator,
 source: []const u8,
 current_position: usize = 0,
 node_buffer: std.ArrayList(Node),
+
+var context: ?ErrorContext = null;
 
 /// `source` must be a managed resource from `allocator`
 pub fn init(comptime grammar: Grammar, allocator: std.mem.Allocator, source: []const u8) Parser {
@@ -59,7 +87,9 @@ pub fn init(comptime grammar: Grammar, allocator: std.mem.Allocator, source: []c
 /// NOTE: If the parser encounters a whitespace which is not handled
 /// by the currently reachable grammar rules then it is ignored.
 pub fn parse(self: *Parser) Error!Tree {
-    const out = try self.parseNode("root");
+    const out = self.parseNode("root") catch |err| {
+        return err;
+    };
     return .{
         .allocator = self.allocator,
         .root = out,
@@ -68,16 +98,42 @@ pub fn parse(self: *Parser) Error!Tree {
     };
 }
 
+pub fn printErrorContext(self: Parser) !void {
+    if (context) |c| {
+        const tmp_node: Node = .{
+            .allocator = self.allocator,
+            .kind = "regex",
+            .start_index = c.source_offset,
+            .end_index = c.source_offset + c.char_count,
+        };
+        std.log.err("failed to parse in node '{s}': {s}", .{ c.node, c.message });
+        const stderr = std.io.getStdErr().writer();
+        const tty_config = std.io.tty.detectConfig(std.io.getStdErr());
+        if (c.regex_expr.len != 0) {
+            _ = try stderr.write("       tried to match regex: '");
+            try std.io.tty.Config.setColor(tty_config, stderr, .green);
+            _ = try stderr.write(c.regex_expr[0..c.regex_offset]);
+            try std.io.tty.Config.setColor(tty_config, stderr, .red);
+            _ = try stderr.write(c.regex_expr[c.regex_offset..]);
+            try std.io.tty.Config.setColor(tty_config, stderr, .reset);
+            _ = try stderr.write("'\n");
+        }
+        try self.printContext(stderr.any(), tmp_node);
+    }
+}
+
 fn parseNode(self: *Parser, kind: []const u8) Error!usize {
     const rule = self.grammar.get(kind) orelse unreachable;
     const pos = self.current_position;
     var out = std.ArrayList(usize).init(self.allocator);
     for (rule.subrules()) |subrule| {
-        const index = try self.parseSubrule(rule, subrule);
-        if (std.mem.eql(u8, self.node_buffer.items[index].kind, "regex")) {
-            self.node_buffer.items[index].kind = kind;
-            return index;
-        }
+        const index = self.parseSubrule(rule, subrule) catch |err| {
+            if (context) |*c| {
+                if (c.node.len == 0)
+                    c.node = kind;
+            }
+            return err;
+        };
         try out.append(index);
     }
     const node: Node = .{
@@ -94,9 +150,11 @@ fn parseNode(self: *Parser, kind: []const u8) Error!usize {
 
 fn parseSubrule(self: *Parser, parent: Grammar.ParsedRule, subrule: Grammar.Subrule) Error!usize {
     return switch (subrule) {
-        .rule => |r| try self.parseNode(r),
-        .buildin => |b| try self.parseBuildin(parent, b),
-        .regex => |r| try self.parseRegex(r),
+        .rule => |r| self.parseNode(r),
+        .buildin => |b| self.parseBuildin(parent, b),
+        .regex => |r| self.parseRegex(r),
+    } catch |err| {
+        return err;
     };
 }
 
@@ -179,7 +237,6 @@ fn parseRegex(
         .succes => |out| {
             const node: Node = .{
                 .kind = "regex",
-                .children = ([0]usize{})[0..],
                 .allocator = self.allocator,
                 .start_index = self.current_position,
                 .end_index = self.current_position + out.len,
@@ -190,9 +247,115 @@ fn parseRegex(
             return index;
         },
         .failure => |f| {
-            std.log.err("failed to match regex: {s}", .{f.message});
+            context = .{
+                .message = f.message,
+                .node = "",
+                .source_offset = self.current_position,
+                .char_count = f.source_offset,
+                .regex_expr = expr,
+                .regex_offset = f.regex_offset - 1,
+            };
             return Error.RegexMatch;
         },
     }
-    return Error.NotImplemented;
+    unreachable;
+}
+
+fn nextLine(self: Parser, node: Node) ?[]const u8 {
+    var pos = node.start_index;
+    while (pos < self.source.len) : (pos += 1) {
+        if (self.source[pos] == '\n') {
+            break;
+        }
+    }
+
+    if (pos == self.source.len)
+        return null;
+
+    const line_begin = pos + 1;
+    var line_end = pos + 1;
+
+    while (line_end < self.source.len) : (line_end += 1) {
+        if (self.source[line_end] == '\n')
+            break;
+    }
+    return self.source[line_begin..line_end];
+}
+
+const LineContext = struct {
+    chars: []const u8,
+    token_offset: usize,
+};
+
+fn currentLine(self: Parser, node: Node) LineContext {
+    var line_begin = node.start_index;
+    var line_end = node.start_index;
+
+    while (line_begin > 0) : (line_begin -= 1) {
+        if (self.source[line_begin] == '\n') {
+            line_begin += 1;
+            break;
+        }
+    }
+
+    while (line_end < self.source.len) : (line_end += 1) {
+        if (self.source[line_end] == '\n')
+            break;
+    }
+    line_begin = if (line_begin > line_end) line_end else line_begin;
+    const tmp = self.source[line_begin..line_end];
+    return .{
+        .chars = tmp,
+        .token_offset = if (tmp.len == 0) 0 else node.start_index - line_begin,
+    };
+}
+
+fn previousLine(self: Parser, node: Node) ?[]const u8 {
+    var pos = node.start_index;
+
+    while (pos > 0) : (pos -= 1) {
+        if (self.source[pos] == '\n') {
+            break;
+        }
+    }
+
+    if (pos == 0)
+        return null;
+
+    var line_begin = pos - 1;
+    const line_end = pos;
+    while (line_begin > 0) : (line_begin -= 1) {
+        if (self.source[line_begin] == '\n') {
+            line_begin += 1;
+            break;
+        }
+    }
+
+    return self.source[line_begin..line_end];
+}
+
+pub fn printContext(self: Parser, out: std.io.AnyWriter, node: Node) !void {
+    const tty_config = std.io.tty.detectConfig(std.io.getStdErr());
+    const line = std.mem.count(u8, self.source[0..node.start_index], "\n") + 1;
+    if (previousLine(self, node)) |previous| {
+        try out.print("{d:5}:{s}\n", .{ line - 1, previous });
+    }
+    const ctxt = currentLine(self, node);
+    const line_chars = ctxt.chars;
+
+    try out.print("{d:5}:{s}", .{ line, line_chars[0..ctxt.token_offset] });
+    if (line_chars.len != 0) {
+        const node_len = node.end_index - node.start_index;
+        try std.io.tty.Config.setColor(tty_config, out, .red);
+        try std.io.tty.Config.setColor(tty_config, out, .bold);
+        try out.print("{s}", .{line_chars[ctxt.token_offset .. ctxt.token_offset + node_len]});
+        try std.io.tty.Config.setColor(tty_config, out, .reset);
+        try out.print("{s}\n", .{line_chars[ctxt.token_offset + node_len ..]});
+    } else {
+        _ = try out.write("\n");
+    }
+
+    if (nextLine(self, node)) |next| {
+        try out.print("{d:5}:{s}\n", .{ line + 1, next });
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -19,27 +19,31 @@ var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = arena.allocator();
 
 pub fn main() !void {
-    const source = try allocator.dupe(u8, "test aoeusnthaoeu");
+    const source = try allocator.dupe(u8, "tes aoeusnthaoeu");
     errdefer allocator.free(source);
     var parser = Parser.init(Grammar.init(G), allocator, source);
-    const tree = try parser.parse();
+    const tree = parser.parse() catch |err| {
+        std.log.err("{}", .{err});
+        try parser.printErrorContext();
+        arena.deinit();
+        std.process.exit(1);
+    };
     defer tree.deinit();
     print_tree(tree);
     std.log.info("\nrest of input: {s}", .{tree.source[parser.current_position..]});
 }
 
 fn print_tree(tree: Parser.Tree) void {
-    const root = tree.nodes[tree.root];
+    const root = tree.node(tree.root);
     std.log.info("{s}", .{root.kind});
     for (root.children) |child_index| {
-        const child = tree.nodes[child_index];
-        print_node(tree, child, 1);
+        print_node(tree, child_index, 1);
     }
 }
 
 fn print_node(
     tree: Parser.Tree,
-    node: Parser.Node,
+    node_index: usize,
     indent: usize,
 ) void {
     const tmp: []u8 = allocator.alloc(u8, 2 * indent) catch @panic("OOM");
@@ -47,11 +51,10 @@ fn print_node(
     for (tmp) |*char| {
         char.* = ' ';
     }
-    std.log.info("{s}{s}:", .{ tmp, node.kind });
-    const chars = tree.source[node.start_index..node.end_index];
-    std.log.info("{s}chars: {s}", .{ tmp, chars });
+    const node = tree.node(node_index);
+    const chars = tree.chars(node_index);
+    std.log.info("{s}{s}: {s}", .{ tmp, node.kind, chars });
     for (node.children) |child_index| {
-        const child = tree.nodes[child_index];
-        print_node(tree, child, indent + 1);
+        print_node(tree, child_index, indent + 1);
     }
 }
